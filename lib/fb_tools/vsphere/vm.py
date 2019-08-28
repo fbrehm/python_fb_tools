999d7d974947b5973c257f104350750c2c5bc6fe
Frank Brehm <frank.brehm@pixelpark.com>
2019-08-28 15:07:03 +0200

    Adding some more properties to class VsphereVm

--- lib/fb_tools/vsphere/vm.py
+++ lib/fb_tools/vsphere/vm.py
@@ -45,6 +45,14 @@ class VsphereVm(VsphereObject):
         self._cluster_name = None
         self._path = None
         self._template = False
+        self._memory_mb = None
+        self._num_cpu = None
+        self._num_ethernet = None
+        self._num_vdisk = None
+        self._guest_fullname = None
+        self._guest_id = None
+        self._uuid = None
+        self._instance_uuid = None
 
         super(VsphereVm, self).__init__(
             name=name, obj_type='vsphere_vm', name_prefix="vm", status=status,
@@ -71,7 +79,7 @@ class VsphereVm(VsphereObject):
     # -----------------------------------------------------------
     @property
     def path(self):
-        """The path of the VM in the iVM folder structure."""
+        """The path of the VM in the VM folder structure."""
         return self._path
 
     @path.setter
@@ -95,6 +103,144 @@ class VsphereVm(VsphereObject):
     def template(self, value):
         self._template = to_bool(value)
 
+    # -----------------------------------------------------------
+    @property
+    def memory_mb(self):
+        """The memory (RAM) of the VM in MiB."""
+        return self._memory_mb
+
+    @memory_mb.setter
+    def memory_mb(self, value):
+        if value is None:
+            self._memory_mb = None
+            return
+        self._memory_mb = int(value)
+
+    # -----------------------------------------------------------
+    @property
+    def memory_gb(self):
+        """The memory (RAM) of the VM in GiB."""
+        if self.memory_mb is None:
+            return None
+        return float(self.memory_mb) / 1024.0
+
+    # -----------------------------------------------------------
+    @property
+    def num_cpu(self):
+        """The number of CPUs of the VM."""
+        return self._num_cpu
+
+    @num_cpu.setter
+    def num_cpu(self, value):
+        if value is None:
+            self._num_cpu = None
+            return
+        self._num_cpu = int(value)
+
+    # -----------------------------------------------------------
+    @property
+    def num_ethernet(self):
+        """The number of virtual ethernet network cards of the VM."""
+        return self._num_ethernet
+
+    @num_ethernet.setter
+    def num_ethernet(self, value):
+        if value is None:
+            self._num_ethernet = None
+            return
+        self._num_ethernet = int(value)
+
+    # -----------------------------------------------------------
+    @property
+    def num_vdisk(self):
+        """The number of virtual disks of the VM."""
+        return self._num_vdisk
+
+    @num_vdisk.setter
+    def num_vdisk(self, value):
+        if value is None:
+            self._num_vdisk = None
+            return
+        self._num_vdisk = int(value)
+
+    # -----------------------------------------------------------
+    @property
+    def guest_fullname(self):
+        """The guest Operating system name."""
+        return self._guest_fullname
+
+    @guest_fullname.setter
+    def guest_fullname(self, value):
+        if value is None:
+            self._guest_fullname = None
+            return
+        v = str(value).strip()
+        if v == '':
+            self._guest_fullname = None
+        else:
+            self._guest_fullname = v
+
+    # -----------------------------------------------------------
+    @property
+    def guest_id(self):
+        """The guest Operating system identifier (shortname)."""
+        return self._guest_id
+
+    @guest_id.setter
+    def guest_id(self, value):
+        if value is None:
+            self._guest_id = None
+            return
+        v = str(value).strip()
+        if v == '':
+            self._guest_id = None
+        else:
+            self._guest_id = v
+
+    # -----------------------------------------------------------
+    @property
+    def uuid(self):
+        """The Virtual machine BIOS identification."""
+        return self._uuid
+
+    @uuid.setter
+    def uuid(self, value):
+        if value is None:
+            self._uuid = None
+            return
+        v = str(value).strip()
+        if v == '':
+            self._uuid = None
+        else:
+            self._uuid = uuid.UUID(v)
+
+    # -----------------------------------------------------------
+    @property
+    def instance_uuid(self):
+        """The VC-specific identifier of the virtual machine."""
+        return self._instance_uuid
+
+    @instance_uuid.setter
+    def instance_uuid(self, value):
+        if value is None:
+            self._instance_uuid = None
+            return
+        v = str(value).strip()
+        if v == '':
+            self._instance_uuid = None
+        else:
+            self._instance_uuid = uuid.UUID(v)
+
+    # -----------------------------------------------------------
+    @property
+    def online(self):
+        """Is this VM generally online or not."""
+#        if self.power_state is None:
+#            return False
+#        if self.power_state.lower() in ('poweredoff', 'unknown'):
+#            return False
+        return True
+
     # -------------------------------------------------------------------------
     def as_dict(self, short=True):
         """
@@ -111,6 +257,16 @@ class VsphereVm(VsphereObject):
         res['cluster_name'] = self.cluster_name
         res['path'] = self.path
         res['template'] = self.template
+        res['online'] = self.online
+        res['memory_mb'] = self.memory_mb
+        res['memory_gb'] = self.memory_gb
+        res['num_cpu'] = self.num_cpu
+        res['num_ethernet'] = self.num_ethernet
+        res['num_vdisk'] = self.num_vdisk
+        res['guest_fullname'] = self.guest_fullname
+        res['guest_id'] = self.guest_id
+        res['uuid'] = self.uuid
+        res['instance_uuid'] = self.instance_uuid
 
         return res
 
@@ -125,6 +281,14 @@ class VsphereVm(VsphereObject):
         vm.cluster_name = self.cluster_name
         vm.path = self.path
         vm.template = self.template
+        vm.memory_mb = self.memory_mb
+        vm.num_cpu = self.num_cpu
+        vm.num_ethernet = self.num_ethernet
+        vm.num_vdisk = self.num_vdisk
+        vm.guest_fullname = self.guest_fullname
+        vm.guest_id = self.guest_id
+        vm.uuid = self.uuid
+        vm.instance_uuid = self.instance_uuid
 
         return vm
 
@@ -172,6 +336,14 @@ class VsphereVm(VsphereObject):
 
         vm.path = cur_path
         vm.template = data.summary.config.template
+        vm.memory_mb = data.summary.config.memorySizeMB
+        vm.num_cpu = data.summary.config.numCpu
+        vm.num_ethernet = data.summary.config.numEthernetCards
+        vm.num_vdisk = data.summary.config.numVirtualDisks
+        vm.guest_fullname = data.summary.config.guestFullName
+        vm.guest_id = data.summary.config.guestId
+        vm.uuid = data.summary.config.uuid
+        vm.instance_uuid = data.summary.config.instanceUuid
 
         if verbose > 2:
             LOG.debug(_("Created {} object:").format(cls.__name__) + '\n' + pp(vm.as_dict()))
