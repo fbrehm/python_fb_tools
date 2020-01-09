d4b764315cff066f18228bbdd4beba954b236fde
Frank Brehm <frank@brehm-online.com>
2020-01-09 07:35:16 +0100

    Adding test script test/test_multicfg.py

---
+++ test/test_multicfg.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@author: Frank Brehm
+@contact: frank@brehm-online.com
+@copyright: © 2020 Frank Brehm, Berlin
+@license: GPL3
+@summary: test script (and module) for unit tests on multi config class
+'''
+
+import os
+import sys
+import logging
+import tempfile
+import datetime
+
+from pathlib import Path
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from babel.dates import LOCALTZ
+
+libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib'))
+sys.path.insert(0, libdir)
+
+from general import FbToolsTestcase, get_arg_verbose, init_root_logger
+
+from fb_tools.common import to_bool
+
+LOG = logging.getLogger('test_multicfg')
+
+
+# =============================================================================
+class TestFbMultiConfig(FbToolsTestcase):
+
+    # -------------------------------------------------------------------------
+    def setUp(self):
+
+        pass
+
+    # -------------------------------------------------------------------------
+    def tearDown(self):
+
+        pass
+
+    # -------------------------------------------------------------------------
+    def test_import(self):
+
+        LOG.info("Testing import of fb_tools.multi_config ...")
+        import fb_tools.multi_config                                        # noqa
+
+
+# =============================================================================
+if __name__ == '__main__':
+
+    verbose = get_arg_verbose()
+    if verbose is None:
+        verbose = 0
+    init_root_logger(verbose)
+
+    LOG.info("Starting tests ...")
+
+    suite = unittest.TestSuite()
+
+    suite.addTest(TestFbMultiConfig('test_import', verbose))
+
+    runner = unittest.TextTestRunner(verbosity=verbose)
+
+    result = runner.run(suite)
+
+
+# =============================================================================
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
