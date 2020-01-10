290dd81df7400844ac10ba79138dff4d2f725cd6
Frank Brehm <frank@brehm-online.com>
2020-01-10 22:42:37 +0100

    Testing config directories

--- test/test_multicfg.py
+++ test/test_multicfg.py
@@ -73,6 +73,33 @@ class TestFbMultiConfig(FbToolsTestcase):
         LOG.debug("BaseMultiConfig %%r: %r", cfg)
         LOG.debug("BaseMultiConfig %%s: %s", str(cfg))
 
+    # -------------------------------------------------------------------------
+    def test_init_cfg_dirs(self):
+
+        LOG.info("Testing init of cofiguration directories.")
+
+        from fb_tools.multi_config import BaseMultiConfig
+
+        cfg = BaseMultiConfig(
+            appname='test_multicfg',
+            config_dir='test', additional_stems='test',
+            verbose=self.verbose,
+        )
+
+        system_path = Path('/etc', 'test')
+        LOG.debug("Testing existence of system config path {!r}.".format(system_path))
+        self.assertIn(system_path, cfg.config_dirs)
+
+        user_path = Path.home() / '.config' / 'test'
+        LOG.debug("Testing existence of user config path {!r}.".format(user_path))
+        self.assertIn(user_path, cfg.config_dirs)
+
+        cwd_etc_dir = Path.cwd() / 'etc'
+        LOG.debug("Testing existence of basedir config path {!r}.".format(cwd_etc_dir))
+        self.assertIn(cwd_etc_dir, cfg.config_dirs)
+
+
+
 # =============================================================================
 if __name__ == '__main__':
 
@@ -87,6 +114,7 @@ if __name__ == '__main__':
 
     suite.addTest(TestFbMultiConfig('test_import', verbose))
     suite.addTest(TestFbMultiConfig('test_object', verbose))
+    suite.addTest(TestFbMultiConfig('test_init_cfg_dirs', verbose))
 
     runner = unittest.TextTestRunner(verbosity=verbose)
 
