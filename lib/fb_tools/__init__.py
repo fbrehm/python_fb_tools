313313577cb3b5b89988747720cc15736b883b5a
Frank Brehm <frank@brehm-online.com>
2024-06-24 18:26:29 +0200

    Fixing lining of lib/fb_tools/__init__.py

--- lib/fb_tools/__init__.py
+++ lib/fb_tools/__init__.py
@@ -7,20 +7,20 @@
 @contact: frank@brehm-online.com
 @copyright: © 2024 by Frank Brehm, Berlin
 """
-# flake8: noqa
 from __future__ import absolute_import
 
-# Own modules
-
-from .mailaddress import MailAddress, QualifiedMailAddress, MailAddressList
-from .multi_config import BaseMultiConfig
+UTF8_ENCODING = 'utf-8'
 
 DDNS_CFG_BASENAME = 'ddns.ini'
 MAX_TIMEOUT = 3600
-UTF8_ENCODING = 'utf-8'
 DEFAULT_ENCODING = UTF8_ENCODING
 DEFAULT_TERMINAL_WIDTH = 99
 DEFAULT_TERMINAL_HEIGHT = 40
+
+# Own modules
+
+from .mailaddress import MailAddress, MailAddressList, QualifiedMailAddress     # noqa: F401
+from .multi_config import BaseMultiConfig                                       # noqa: F401
 
 __version__ = '2.6.0'
 
