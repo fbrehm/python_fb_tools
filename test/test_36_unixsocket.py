905b28db2f032bfc05a9635ff6d0423aab1b5145
Frank Brehm <frank@brehm-online.com>
2024-10-30 15:36:50 +0100

    Adding and using test/listener_thread.py

--- test/test_36_unixsocket.py
+++ test/test_36_unixsocket.py
@@ -8,17 +8,16 @@
 @copyright: © 2024 Frank Brehm, Berlin
 @license: GPL3
 """
+from __future__ import absolute_import
 
 import logging
 import os
-import queue
 import stat
 import sys
 import tempfile
 import textwrap
 import time
 from pathlib import Path
-from threading import Thread
 
 try:
     import unittest2 as unittest
@@ -32,70 +31,6 @@ from general import FbToolsTestcase, get_arg_verbose, init_root_logger
 
 APPNAME = 'test_unix_socket'
 LOG = logging.getLogger(APPNAME)
-
-
-# =============================================================================
-class ListenerThread(Thread):
-    """A Thread for listening on a socket file."""
-
-    # -------------------------------------------------------------------------
-    def __init__(self, listener_sock, msg2read, *args, **kwargs):
-        """Initialize the thread."""
-        self.listener_sock = listener_sock
-        self.msg2read = msg2read
-        self.listener_sock.bind()
-        self.__status_queue = queue.Queue()
-        super(ListenerThread, self).__init__(*args, **kwargs)
-
-    # -------------------------------------------------------------------------
-    def run(self):
-        """Read from socket and log the result."""
-        max_attempts = 100
-        cur_attempt = 0
-        line = None
-
-        try:
-            while cur_attempt < max_attempts:
-                cur_attempt += 1
-                if self.listener_sock.verbose > 2:
-                    LOG.debug('Attempt {nr} on reading from socket {s!r} ...'.format(
-                        nr=cur_attempt, s=str(self.listener_sock.filename)))
-
-                if self.listener_sock.has_data():
-                    line = self.listener_sock.read_line()
-                    break
-
-            if line:
-                LOG.debug('Got line from socket after {at} attempts: {li!r}'.format(
-                    at=cur_attempt, li=line))
-                if line != self.msg2read:
-                    msg = 'Read wrong content - expected: {ex!r}, got {got!r}.'.format(
-                        ex=self.msg2read, got=line)
-                    raise RuntimeError(msg)
-
-            else:
-                msg = 'Got not data from socket {s!r} after {at} reading attempts.'.format(
-                    s=str(self.listener_sock.filename), at=cur_attempt)
-                raise RuntimeError(msg)
-
-        except BaseException:
-            self.__status_queue.put(sys.exc_info())
-
-        self.__status_queue.put(None)
-
-    # -------------------------------------------------------------------------
-    def wait_for_exc_info(self):
-        """Return content of status quue."""
-        return self.__status_queue.get()
-
-    # -------------------------------------------------------------------------
-    def join_with_exception(self):
-        """Return, if there was no exception, otherwise reraise it."""
-        ex_info = self.wait_for_exc_info()
-        if ex_info is None:
-            return
-        else:
-            raise ex_info[1]
 
 
 # =============================================================================
@@ -192,6 +127,7 @@ class TestUnixSocketHandler(FbToolsTestcase):
         self.socketfile.unlink()
 
         from fb_tools.socket_obj.unix import UnixSocket
+        from listener_thread import ListenerThread
 
         msg2send = 'Hallo Ballo!\n'
 
